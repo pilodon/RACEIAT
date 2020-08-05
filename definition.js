0d391f0a1322d7df04fbed647148c3b6ebbbad0d
pilodon <69127941+pilodon@users.noreply.github.com>
2020-08-05 18:37:10 -0400

    Create definition.js

---
+++ definition.js
@@ -0,0 +1,45 @@
+define(['pipAPI', 'https://pilodon.github.io/RACEIAT/definition.js/mainIAT.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'Black people', //Will appear in the data.
+			title : {
+				media : {word : 'Black people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'black1.jpg'}, 
+    			{image : 'black2.jpg'}, 
+    			{image : 'black3.jpg'}, 
+    			{image : 'black4.jpg'}, 
+    			{image : 'black5.jpg'}, 
+    			{image : 'black6.jpg'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'White people', //Will appear in the data.
+			title : {
+				media : {word : 'White people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'white1.jpg'}, 
+    			{image : 'white2.jpg'}, 
+    			{image : 'white3.jpg'}, 
+    			{image : 'white4.jpg'}, 
+    			{image : 'white5.jpg'}, 
+    			{image : 'white6.jpg'}			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+
+		base_url : {//Where are your images at?
+			image : 'https://github.com/pilodon/IATRACE/'
+		} 
+	});
+});
